25e5cd83b53dc7daf5c0c00a39eaffca08c0c7df
Your Name <gjlebx@gmail.com>
2020-02-03 03:47:33 +0300

    init commit

---
+++ src/main/java/HW/rest/GreetingController.java
@@ -0,0 +1,24 @@
+package HW.rest;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+//    @RequestMapping("/greeting")
+//    public Hello greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+//        return new Hello(counter.incrementAndGet(),
+//                String.format(template, name));
+//    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public Hello addTopic (@RequestBody Map<String, Long> json){
+        return new Hello(counter.incrementAndGet(),"GJLEB", json.get("number"));
+    }
+}
