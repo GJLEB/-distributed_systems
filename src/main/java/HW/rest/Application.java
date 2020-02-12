249636c7fd240ab8bad7117faee56f97b93d559e
Your Name <gjlebx@gmail.com>
2020-02-12 08:13:27 +0300

    Docker added

--- src/main/java/HW/rest/Application.java
+++ src/main/java/HW/rest/Application.java
@@ -1,5 +1,6 @@
 package HW.rest;
 
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @ComponentScan
+@SpringBootConfiguration
 @EnableAutoConfiguration
 @EnableJpaAuditing
 @EnableTransactionManagement
